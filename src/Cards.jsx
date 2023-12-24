ba29c2d7d111aeb5d9c16c11612d23d729ed5d8e
aditi <additigupta366@gmail.com>
2023-12-24 22:54:32 +0530

    done

---
+++ src/Cards.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Card(props){
+    console.log(props);
+    const link="https://babeljs.io/repl#?browsers=defaults%2C%20not%20ie%2011%2C%20not%20ie_mob%2011&build=&builtIns=false&corejs=3.21&spec=false&loose=false&code_lz=EoUwhgxgLgIg8gWQHQCcQDsAmIUAoA8AFgIwB86YAtmAM5QgAEmArugJYCeY-A9CaQBpMAewjNKGKEgDmIKAFEANiAnooAIQ4BJTLgDkKYcKh6AlKYDcAKCtA&debug=false&forceAllTransforms=false&modules=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=env%2Creact%2Cstage-2&prettier=true&targets=&version=7.23.6&externalPlugins=&assumptions=%7B%7D";
+    return(
+      <>
+     
+        <div className="cards">
+        <div className="card">
+          <img src={props.imgsrc} alt="pic1" className="card-img"/>
+          <div className="info">
+            <span className="category">{props.title}</span>
+            <h3 className="title">{props.sname}</h3>
+            <a href={link} target="_djbj">
+              <button>Watch Now</button>
+            </a>
+          </div>
+        </div>
+      </div>
+      </>
+    );
+}
+
+export default Card;
---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata=[
+    {
+        sname:"DARK",
+        imgsrc:"https://picsum.photos/id/237/200/300", title:"a netflix original series",
+        
+    },
+    {
+        sname:"SUPERMAN",
+        imgsrc:"https://picsum.photos/id/247/200/300", title:"a netflix original series",
+        
+    },
+    {
+        sname:"BATMAN",
+        imgsrc:"https://picsum.photos/id/250/200/300", title:"a netflix original series",
+        
+    },
+    {
+        sname:"DARK",
+        imgsrc:"https://picsum.photos/id/237/200/300", title:"a netflix original series",
+        
+    },
+    {
+        sname:"DARK",
+        imgsrc:"https://picsum.photos/id/237/200/300", title:"a netflix original series",
+        
+    },
+    {
+        sname:"BATMAN",
+        imgsrc:"https://picsum.photos/id/250/200/300", title:"a netflix original series",
+        
+    }
+];
+export default Sdata;
+
